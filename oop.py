9fdfe562b439fd114d24a957006ae5b7eb990fc5
rianaditro <rianaditro@gmail.com>
2024-03-15 14:39:51 +0700

    refac: create parsing class

--- oop.py
+++ oop.py
@@ -1,7 +1,7 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
-import json,pandas,re
+import json,pandas,re, concurrent.futures
 
 
 class Scraper:
@@ -14,9 +14,25 @@ class Scraper:
         html = send_requests.html.html
         return html
     
-    def parse_script(self,html):
+    def extract_links(self,url):
+        send_requests = self.session.get(url)
+        send_requests.html.render(sleep=1,scrolldown=15)
+        html = send_requests.html.html
         soup = BeautifulSoup(html,"html.parser")
-        script = soup.find("script",{"type":"application/ld+json"}).text.strip()
+
+        links = soup.find_all("a",class_=re.compile("product search-product product-info-container bw-plp-product-card"))
+        all_links = [f'https://www.beerwulf.com{link["href"]}' for link in links if "beercases" not in link["href"]]
+        print(f"Get {len(all_links)} of links")
+
+        return all_links
+
+class Parser:
+    def __init__(self,html):
+        soup = BeautifulSoup(html,"html.parser")
+        self.soup = soup
+    
+    def parse_script(self):
+        script = self.soup.find("script",{"type":"application/ld+json"}).text.strip()
         script = json.loads(script)
         name = script["name"]
         image = script["image"]
@@ -40,10 +56,9 @@ class Scraper:
         }
         return product_details
     
-    def parse_table_details(self,html):
+    def parse_table_details(self):
         table_details = dict()
-        soup = BeautifulSoup(html,"html.parser")
-        table = soup.find_all("td")
+        table = self.soup.find_all("td")
         for i in range(0,len(table),2):
             key = table[i].text.strip()
             if key == "Allergens" or key == "Hops" or key == "NutritionalValues":
@@ -54,11 +69,10 @@ class Scraper:
             i = i+2
         return table_details
     
-    def parse_taste_tables(self,html):
+    def parse_taste_tables(self):
         taste_tables = dict()
-        soup = BeautifulSoup(html,"html.parser")
         try:
-            taste_tables_html = soup.find("div","taste-tables")
+            taste_tables_html = self.soup.find("div","taste-tables")
             div = taste_tables_html.find_all("div")
             i = 0
             while i in range(len(div)):   
@@ -73,36 +87,30 @@ class Scraper:
 
         return taste_tables
 
-    def details(self,url,html):
-        product_details = self.parse_script(html)
-        table_details = self.parse_table_details(html)
-        taste_tables = self.parse_taste_tables(html)
-        details = {"ProductURL":url}
-        details = {**product_details,**table_details,**taste_tables,**details}
+    def details(self):
+        product_details = self.parse_script()
+        table_details = self.parse_table_details()
+        taste_tables = self.parse_taste_tables()
+        details = {**product_details,**table_details,**taste_tables}
         return details
-
-    def extract_links(self,url):
-        send_requests = self.session.get(url)
-        send_requests.html.render(sleep=1,scrolldown=15)
-        html = send_requests.html.html
-        soup = BeautifulSoup(html,"html.parser")
-
-        links = soup.find_all("a",class_=re.compile("product search-product product-info-container bw-plp-product-card"))
-        all_links = [f'https://www.beerwulf.com{link["href"]}' for link in links if "beercases" not in link["href"]]
-        print(f"Get {len(all_links)} of links")
-
-        return all_links
     
-    def save(self,data,filename):
-        df = pandas.DataFrame(data)
-        df.to_excel(filename,index=False)
-        print("Saved!")
+def save_to_excel(data,filename="beer_product.xlsx"):
+    df = pandas.DataFrame(data)
+    df.to_excel(filename,index=False)
+    print(f"{filename} Saved!")
 
+def main(url):
+    result = []
+    beer = Scraper()
+    links = beer.extract_links(url)
+    for link in links:
+        html = beer.get_html(link)
+        data = Parser(html).details()
+        data["ProductURL"] = link
+        result.append(data)
+    save_to_excel(result)
+    
 
 if __name__=="__main__":
     url = "https://www.beerwulf.com/en-gb/c/beer-kegs"
-    beer = Scraper()
-    links = beer.extract_links(url)
-    items = [beer.details(link,beer.get_html(link)) for link in links]    
-    
-    beer.save(items,"beer_product.xlsx")
+    main(url)
