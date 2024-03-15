69f1643a78dbec851a9e206a4bf2d1c7f9eaa82e
rianaditro <rianaditro@gmail.com>
2024-03-15 10:52:54 +0700

    refac: split details into 3 func

--- oop.py
+++ oop.py
@@ -13,49 +13,64 @@ class Scraper:
         print(f"Get {url}")
         html = send_requests.html.html
         return html
-
-    def details(self,html):
-        item = {
-            "ProductUrl":url,
-            "Title":"",
-            "Price":"",
-            "PriceCurrency":"",
-            "Rating":"",
-            "ReviewCount":"",
-            "Availability":"",
-            "Style":"",
-            "Volume":"",
-            "ABV":"",
-            "Malt":"",
-            "Origin":"",
-            "Brewer":"",
-            
-        }
-
+    
+    def parse_script(self,html):
         soup = BeautifulSoup(html,"html.parser")
-        item["Title"] = soup.find("span","bw-text-h2 bw-mb-3").text.strip()
-
+        script = soup.find("script",{"type":"application/ld+json"}).text.strip()
+        script = json.loads(script)
+        name = script["name"]
+        image = script["image"]
+        rating = script["aggregateRating"]["ratingValue"]
+        review = script["aggregateRating"]["reviewCount"]
+        price = script["offers"]["price"]
+        currency = script["offers"]["priceCurrency"]
+        availability = script["offers"]["availability"]
+        if availability == "http://schema.org/InStock":
+            availability = "In Stock"
+        else:
+            availability = "Out of Stock"
+        product_details = {
+            "ProductName":name,
+            "Image":image,
+            "Rating":rating,
+            "ReviewCount":review,
+            "Price":price,
+            "PriceCurrency":currency,
+            "Availability":availability
+        }
+        return product_details
+    
+    def parse_table_details(self,html):
+        table_details = dict()
+        soup = BeautifulSoup(html,"html.parser")
         table = soup.find_all("td")
         for i in range(0,len(table),2):
             key = table[i].text.strip()
-            if key == "Brewer" or key == "Allergens" or key == "Hops":
-                val = table[i+1].text.split()
+            if key == "Allergens" or key == "Hops" or key == "NutritionalValues":
+                pass
             else:
                 val = table[i+1].text.replace("\n","").replace("\r","").strip()
-            item[key] = val
+                table_details[key] = val
             i = i+2
-        
-        script = soup.find("script",{"type":"application/ld+json"}).text.strip()
-        script = json.loads(script)
-        item["Price"] = script["offers"]["price"]
-        item["Rating"] = script["aggregateRating"]["ratingValue"]
-        availability = script["offers"]["availability"]
-        if availability == "http://schema.org/InStock":
-            item["Availability"] = "In Stock"
-        else:
-            item["Availability"] = "Out of Stock"
-        
-        return item
+        return table_details
+    
+    def parse_taste_tables(self,html):
+        taste_tables = dict()
+        soup = BeautifulSoup(html,"html.parser")
+        taste_tables_html = soup.find("div","taste-tables")
+        div = taste_tables_html.find_all("div")
+        i = 0
+        while i in range(len(div)):   
+            name = div[i].section.span.text.strip()
+            value = div[i].section.div["title"]
+            taste_tables[name]=value
+            i = i+3
+            if i > 15:
+                break
+        return taste_tables
+
+    def details(self,html):
+        pass
 
     def extract_links(self,url):
         send_requests = self.session.get(url)
@@ -86,17 +101,9 @@ if __name__=="__main__":
     url = "https://www.beerwulf.com/en-gb/p/beers/affligem-blond-2l-keg"
     beer = Scraper()
     html = beer.get_html(url)
-    soup = BeautifulSoup(html, "html.parser")
-    taste_tables = soup.find("div","taste-tables")
-    div = taste_tables.find_all("div")
-    print(len(div))
-    i = 0
-    while i in range(len(div)):
-        print(i)       
-        name = div[i].section.span.text.strip()
-        value = div[i].section.div["title"]
-        print(name)
-        print(value)
-        i = i+3
-        if i > 15:
-            break
+    data1 = beer.parse_script(html)
+    data2 = beer.parse_table_details(html)
+    data3 = beer.parse_taste_tables(html)
+    print(data1)
+    print(data2)
+    print(data3)
